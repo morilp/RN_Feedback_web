e0e7fc4b3da0ccb82e19ebafa40f8837c24a014e
mori92 <mori92@duck.com>
2024-01-05 17:04:17 +0330

    Add passport library to be ready for OAuth third-party authentications.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require("express");
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
 app.get('/', (req, res)=>{
     res.send({bye: 'dude'});
 });
+
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 
