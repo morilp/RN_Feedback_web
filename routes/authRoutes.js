c7268ff00216b40eba72a5459d4a6e6370998f46
mori92 <mori92@duck.com>
2024-01-07 23:58:53 +0330

    initial google oauth20 with passport

---
+++ routes/authRoutes.js
@@ -0,0 +1,24 @@
+// Import Passport for authentication
+const passport = require('passport');
+
+
+// Exporting a function to work with Express applications in the root directory of index.js
+module.exports = (app) => {
+    // Initiate Google Account Login and handle authentication information retrieval
+    app.get(
+        '/auth/google',
+        passport.authenticate(
+            'google',
+            {
+                // Define the data we want to access from the Google client
+                scope: ['profile', 'email']
+            }
+        )
+    );
+
+    // After Google validates the client, redirect to this endpoint to effectively manage authentication data
+    app.get(
+        '/auth/google/callback',
+        passport.authenticate('google')
+    );
+};
