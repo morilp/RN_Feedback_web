2a900ee157b08752700a635c4520016d96652a09
mori92 <mori92@duck.com>
2024-01-10 21:50:04 +0330

    fix proxy auth problem

--- services/passport.js
+++ services/passport.js
@@ -31,7 +31,8 @@ passport.use(
         {
             clientID: key.googleClientID,
             clientSecret: key.googleClientSecret,
-            callbackURL: '/auth/google/callback'
+            callbackURL: '/auth/google/callback',
+            proxy: true
         },
         (accessToken, refreshToken, profile, done) => {
             User.findOne({ googleId: profile.id })
